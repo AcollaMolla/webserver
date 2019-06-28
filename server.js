2581ebaffb24a7b4ed86377aeab74a284b613437
Anton <anton_engman@hotmail.com>
2019-06-28 09:42:17 +0200

    initial commit

---
+++ server.js
@@ -0,0 +1,121 @@
+const express = require('express');
+const app = express();
+
+var mongo = require('mongodb');
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/db";
+
+app.use(express.json());
+app.use(function(req, res, next){
+        // Website you wish to allow to connect
+        res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+
+        // Request methods you wish to allow
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    
+        // Request headers you wish to allow
+        res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    
+        // Set to true if you need the website to include cookies in the requests sent
+        // to the API (e.g. in case you use sessions)
+        res.setHeader('Access-Control-Allow-Credentials', true);
+    
+        // Pass to next layer of middleware
+        next();
+});
+let settings = [
+    {
+        "backgroundColor" : "#282c34",
+        "isImage" : "false",
+        "url" : "null"
+    }
+]
+
+let fish = [
+    {
+        "species" : "Abborre",
+        "catchDate" : "2019-06-06",
+        "length" : "15",
+        "weight" : "0.5"
+    }
+]
+
+app.get('/', (req,res) => {
+    res.send("hej");
+});
+
+app.get('/settings', (req, res) => {
+    res.send(settings);
+});
+
+app.post('/settings', (req, res) => {
+    settings = [
+        {
+            backgroundColor : req.body.backgroundColor,
+            isImage: req.body.isImage,
+            url: req.body.url
+        }
+    ]
+    res.send(settings);
+});
+
+app.get('/stocks', (req, res) => {
+    getFromDb("stocks", function(callback){
+        res.send(callback);
+    })
+});
+
+app.post('/stocks', (req, res) => {
+    const stock = {
+        name: req.body.name,
+        buyDate: req.body.buyDate,
+        sellDate: req.body.sellDate
+    };
+    postToDb(stock, "stocks");
+    res.send(stock);
+});
+
+app.get('/fishing', (req, res) => {
+    getFromDb("fishing", function(callback){
+        res.send(callback);
+    })
+});
+
+app.post('/fishing', (req, res) => {
+    const fish = {
+        species: req.body.species,
+        catchDate: req.body.catchDate,
+        weight: req.body.weight,
+        length: req.body.length
+    }
+    postToDb(fish, "fishing");
+    res.send(fish);
+})
+
+var server = app.listen(8081, function () {
+    console.log("Example app listening at localhost:8081");
+ })
+
+function postToDb(record, collection){
+    MongoClient.connect(url, function(err, db){
+        if(err)throw err;
+        var dbo = db.db("db");
+        dbo.collection(collection).insertOne(record, function(err, res){
+            if(err)throw err;
+            console.log("1 doc added");
+            db.close();
+        });
+    });
+ }
+
+function getFromDb(collection, callback){
+    MongoClient.connect(url, function(err, db){
+        var dbo = db.db("db");
+        dbo.collection(collection).find({}).toArray(function(err, result){
+            if(err)throw err;
+            console.log("Fetched from db");
+            callback(result);
+            db.close();
+        });
+    });
+ }
