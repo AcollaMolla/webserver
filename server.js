bb62a67d6624c1f0530dfb415e3efd74f4130831
Exekveras <antengman@gmail.com>
2019-07-04 20:16:28 +0200

    File upload working

--- server.js
+++ server.js
@@ -111,7 +111,15 @@ app.get('/images', (req, res) => {
 });
 
 app.post('/images', (req, res) => {
-    console.log("Got request: " + req.files);
+    upload(req, res, function(err){
+        if(err instanceof multer.MulterError){
+            return res.status(500).json(err)
+        }
+        else if(err){
+            return res.status(500).json(err)
+        }
+        return res.status(200).send(req.file)
+    })
 });
 
 var server = app.listen(8081, function () {
